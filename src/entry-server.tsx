0d8ba81caf8e1aa566f6b038d56c80b65c14347c
CryUshio <1151291182@qq.com>
2024-03-15 22:44:00 +0800

    feat: seo optimize

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,7 +6,12 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
+          <title>Regex Generator AI - Automatically generate regular expressions.</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Harness the capabilities of GPT to effortlessly create regular expressions on demand. This tool focuses on simplifying text pattern matching by automatically generating regex code from your input descriptions. Achieve accurate results quickly and efficiently, without the complexity of manual regex crafting."
+          />
           <link rel="icon" href="/favicon.png" />
           {assets}
         </head>
