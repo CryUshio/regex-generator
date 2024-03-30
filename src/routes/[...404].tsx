aca2fdfe200b8b081dbc523c24d705088291a73d
CryUshio <1151291182@qq.com>
2024-03-30 20:59:25 +0800

    feat: optimize font load

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,8 +3,8 @@ import { A } from '@solidjs/router';
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl font-thin uppercase my-16">Not Found</h1>
-      <p class="my-4">
+      <h1 class="max-6-xs text-5xl mt-16 mb-8 font-medium font-jost select-none">Not Found</h1>
+      <p class="my-4 font-jost">
         <A href="/" class="underline">
           Home Page
         </A>
